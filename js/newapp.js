c061fdaf17d8f379957347daa32ea83b3a04b1bf
Denzel Wamburu <denzjoseph@gmail.com>
2016-07-13 05:26:49 +0300

    [Feature]Include Service for GET Humidities

--- js/newapp.js
+++ js/newapp.js
@@ -12,7 +12,20 @@ app.factory('MoisturesService', function($firebaseArray, $firebaseObject) {
     }
   }
 })
-app.controller("MainCtrl", function($scope, MoisturesService) {
+app.factory('HumiditiesService', function($firebaseArray, $firebaseObject) {
+
+  var ref = new Firebase('https://nelionfarm.firebaseio.com');
+
+  return {
+    getHumidities: function() {
+      return $firebaseArray(ref.child('humidities'));
+    },
+    getHumidity: function(humidityId) {
+      return $firebaseObject(ref.child('humidities').child(humidityId));
+    }
+  }
+})
+app.controller("MainCtrl", function($scope, MoisturesService HumiditiesService) {
   // var ref = new Firebase("https://nelionfarm.firebaseio.com");
 
   // download the data into a local object
@@ -23,5 +36,9 @@ app.controller("MainCtrl", function($scope, MoisturesService) {
   // console.log("File is on");
   // putting a console.log here won't work, see below
   $scope.moistures = MoisturesService.getMoistures();
-console.log($scope.moistures);
+  console.log('Moistures Data is :' + $scope.moistures);
+
+  // GET humidities
+  $scope.humidities = HumiditiesService.getHumidities();
+  console.log('The Humidities Data is :' + $scope.humidities);
 });
