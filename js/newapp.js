7f1188715df850400740cc5c63abcc9ab1ca66c2
Denzel Wamburu <denzjoseph@gmail.com>
2016-08-09 08:30:55 +0300

    Populate Humidities array

--- js/newapp.js
+++ js/newapp.js
@@ -72,6 +72,127 @@ app.controller("MainCtrl", function($scope, TemperaturesService, HumiditiesServi
             arrayplay.push(humidities[i].average);
         // console.log(arrayplay);
         $scope.humiditiesplay = arrayplay;
+        var timeplay = [];
+        for (var i = 0; i < n; i++)
+            timeplay.push(temperatures[i].timestamp);
+        // console.log(arrayplay);
+        $scope.humiditiestime = timeplay;
+        // Time Slicing
+        var timepoop = timeplay.slice(1).slice(-24);
+        var inverttimepoop = timepoop.reverse();
+        console.log(inverttimepoop);
+
+        // Do some slicing here
+        // var poop = arrayplay.slice(Math.max(arrayplay.length - 5, 1))
+        var poop = arrayplay.slice(1).slice(-24);
+        var pooped = poop.reverse();
+
+        // humidities array population
+        console.log(poop);
+        $scope.labels = ["6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM"];
+        $scope.label = [inverttimepoop];
+        $scope.series = ['Humidities'];
+        $scope.humiditydata = [
+            pooped.reverse()
+        ];
+        $scope.seriesed = ["Humidities"]
+        $scope.humdatad = $scope.humiditiesplay;
+        console.log($scope.humdatad.length);
+
+
+        var now = moment().get('hour');
+        console.log(now);
+
+        // i couldn't think so u had to type this whole else if 23 times :(
+        if (now == 0) {
+            $scope.times = ["1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM"];
+        } else if (now == 1) {
+            $scope.times = ["2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM"];
+
+        } else if (now == 2) {
+            $scope.times = ["3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM"];
+
+        } else if (now == 3) {
+            $scope.times = ["4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM"];
+
+        } else if (now == 4) {
+            $scope.times = ["5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM"];
+
+        } else if (now == 5) {
+            $scope.times = ["6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM"];
+
+        } else if (now == 6) {
+            $scope.times = ["7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM"];
+
+        } else if (now == 7) {
+            $scope.times = ["8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM"];
+
+        } else if (now == 8) {
+            $scope.times = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM",
+                "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM"
+            ];
+
+        } else if (now == 9) {
+            $scope.times = ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM",
+                "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM"
+            ];
+        } else if (now == 10) {
+            $scope.times = ["11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM",
+                "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM"
+            ];
+        } else if (now == 11) {
+            $scope.times = ["12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM",
+                "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM"
+            ];
+        } else if (now == 12) {
+            $scope.times = ["1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM",
+                "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM"
+            ];
+        } else if (now == 13) {
+            $scope.times = ["2 PM", "3 PM", "4 PM", "5 PM", "6 PM",
+                "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM"
+            ];
+        } else if (now == 14) {
+            $scope.times = ["3 PM", "4 PM", "5 PM", "6 PM",
+                "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM"
+            ];
+        } else if (now == 15) {
+            $scope.times = ["4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM"];
+        } else if (now == 16) {
+            $scope.times = ["5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM"];
+        } else if (now == 17) {
+            $scope.times = ["6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+        } else if (now == 18) {
+            $scope.times = ["7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM"];
+        } else if (now == 19) {
+            $scope.times = ["8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM"];
+        } else if (now == 20) {
+            $scope.times = ["9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", " PM"];
+
+        } else if (now == 21) {
+            $scope.times = ["10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM"];
+
+        } else if (now == 22) {
+            $scope.times = ["11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM"];
+
+        } else if (now == 23) {
+            $scope.times = ["12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM"];
+
+        } else {
+            $scope.times = ["6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM"];
+
+        }
+        // console.log($scope.times);
+        var _reversetimes = $scope.times;
+        console.log(_reversetimes);
+        _reversetimes.reverse();
+        // console.log(reversetimes);
+        console.log(_reversetimes);
+        $scope.datad = $scope.times.reverse();
+
+
+
+
     });
 
     //array push for temperature
@@ -207,10 +328,10 @@ app.controller("MainCtrl", function($scope, TemperaturesService, HumiditiesServi
         $scope.datad = $scope.times.reverse();
     });
 
-// $scope.previousmonth = moment().subtract(1, 'months');
-var ans = moment().months(); // Number
-$scope.previousmonth = ans - 1;
+    // $scope.previousmonth = moment().subtract(1, 'months');
+    var ans = moment().months(); // Number
+    $scope.previousmonth = ans - 1;
 
-console.log(ans);
+    console.log(ans);
 
 });
